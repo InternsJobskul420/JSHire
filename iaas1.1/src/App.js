500d396fe44bdb99fa6fa9d131c6d73fc4e8e9e4
nishiteshSomething <nishiteshpadhi14@gmail.com>
2023-05-18 13:14:10 +0530

    file strucutre fixed

--- iaas1.1/src/App.js
+++ iaas1.1/src/App.js
@@ -1,9 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NavigationBar from './components/NavigationBar/NavigationBar';
+import Login from './components/Login/Login';
+
 
 function App() {
   return (
-  <div>hello</div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<NavigationBar/>}/>
+        <Route path='/login' element={<Login/>}/>
+       
+      </Routes>
+    </Router>
   );
 }
 
