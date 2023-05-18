e76e0aeccf6f380b88fa18de9cc7677d5f7408e6
nishiteshSomething <nishiteshpadhi14@gmail.com>
2023-05-18 13:58:59 +0530

    js file edit

--- iaas1.1/src/components/Login/Login.js
+++ iaas1.1/src/components/Login/Login.js
@@ -2,7 +2,35 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>hekllo</div>
+    <div className={styles.rightSide}>
+    <div className={styles.textCenter}>
+      <h1 className={`${styles.heading} ${styles.mb5}`}>Admin Console</h1>
+    </div>
+    <form>
+      <div className={styles.mb4}>
+        <label className={styles.label}>Email address</label>
+        <input
+          className={`${styles.inputField} ${styles.formControl}`}
+          type="email"
+          name="email"
+        />
+      </div>
+      <div className={styles.mb4}>
+        <label className={styles.label}>Password</label>
+        <input
+          className={`${styles.inputField} ${styles.formControl}`}
+          type="password"
+          name="password"
+        />
+      </div>
+      <div className={`${styles.textCenter} ${styles.mb4}`}>
+        <a href="!#" className={styles.myfont}>Forgot password?</a>
+      </div>
+      <div className={styles.textCenter}>
+        <button type="submit" className={styles.submitBtn}>Submit</button>
+      </div>
+    </form>
+  </div>
   )
 }
 
