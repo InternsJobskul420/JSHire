61f8d3519881be94d688b5a3e27d55803a6813b7
nishiteshSomething <nishiteshpadhi14@gmail.com>
2023-05-19 17:41:00 +0530

    new changes

--- iaas1.1/src/interfaces/Admin/HiringCompany/JobOpeningForm.js
+++ iaas1.1/src/interfaces/Admin/HiringCompany/JobOpeningForm.js
@@ -8,35 +8,35 @@ export const JobOpeningForm = () => {
     <NavigationBar/>
     <h1 className={styles.formHeading}>Add Opening</h1>
     <form>
-      <div className="form-HCcontainer">
-        <div className="column1">
-          <label htmlFor="jobRole">Job Role</label>
-          <input type="text" id="jobRole" className="inputFieldHC" />
+      <div className="form-container">
+        <div className="form-column">
+          <label htmlFor="jobRole" className="form-label">Job Role</label>
+          <input type="text" id="jobRole" className="form-input" />
         
-          <label htmlFor="companyName">Company Name</label>
-          <input type="text" id="companyName" className="inputFieldHC" />
+          <label htmlFor="companyName" className="form-label">Company Name</label>
+          <input type="text" id="companyName" className="form-input" />
         
-          <label htmlFor="companyDescription">Company Description</label>
-          <textarea id="companyDescription" className="inputFieldBigHC"></textarea>
+          <label htmlFor="companyDescription" className="form-label">Company Description</label>
+          <textarea id="companyDescription" className="form-input doubleHeight"></textarea>
         
-          <label htmlFor="numberOfOpenings">Number of Openings</label>
-          <input type="number" id="numberOfOpenings" className="inputFieldHC" />
+          <label htmlFor="numberOfOpenings" className="form-label">Number of Openings</label>
+          <input type="number" id="numberOfOpenings" className="form-input" />
         </div>
         
-        <div className="column2">
-          <label htmlFor="jobDescription">Job Description</label>
-          <textarea id="jobDescription" className="inputFieldBigHC"></textarea>
+        <div className="form-column">
+          <label htmlFor="jobDescription" className="form-label">Job Description</label>
+          <textarea id="jobDescription" className="form-input doubleHeight"></textarea>
         
-          <label htmlFor="jobRequirement">Job Requirement</label>
-          <textarea id="jobRequirement" className="inputFieldBigHC"></textarea>
+          <label htmlFor="jobRequirement" className="form-label">Job Requirement</label>
+          <textarea id="jobRequirement" className="form-input doubleHeight"></textarea>
         
-          <label htmlFor="basicQualification">Basic Qualification</label>
-          <textarea id="basicQualification" className="inputFieldBigHC"></textarea>
-          
-          <button type="submit" className="btn">Submit Now</button>
+          <label htmlFor="basicQualification" className="form-label">Basic Qualification</label>
+          <textarea id="basicQualification" className="form-input doubleHeight"></textarea>
         </div>
       </div>
-      </form>
+      
+      <button type="submit" className="form-button">Submit Now</button>
+    </form>
     </>
   )
 }
