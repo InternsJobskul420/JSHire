848abfa9b83c784655043e88d04294ecb4805b14
Ishita <ishitamun2000@gmail.com>
2023-05-20 12:10:50 +0530

    EndInterview

--- iaas1.1/src/interfaces/Candidate/EndInterview.js
+++ iaas1.1/src/interfaces/Candidate/EndInterview.js
@@ -1,36 +1,55 @@
-import React from 'react'
-import CandidateMessage from '../../components/CandidateMessage/CandidateMessage'
+import React, { useEffect, useState } from 'react'
+
 import { useLocation } from 'react-router-dom'
+import CandidateShowMessage from '../../components/CandidateShowMessage/CandidateShowMessage';
+import styles from './Candidate.module.css'
 
 const EndInterview = () => {
+  const heading = "Congratulations!";
+  const description = "You have successfully completed the interview :)";
+  const [seconds, setSeconds] = useState(5);
+  const [uploadMessage, setUploadMessage] = useState("Wait for a few seconds while we finish uploading your videos");
 
-    const location = useLocation();
-    const currentPath = location.pathname;
-    const regex = /^\/(\d+)\/.*$/;
-    let number;
-    const match = currentPath.match(regex);
-    console.log(match)
-    console.log(match[1])
 
-    if (match && match[1]) {
-      number = parseInt(match[1]);
+  useEffect(() => {
+    if (seconds === 0) {
+      setUploadMessage('Your Data has been successfully uploaded');
     }
+  }, [seconds]);
 
-    if (number === 2) {
-      console.log("Displaying message for number 2");
-      // Perform the desired action when the number is 2
-    } else {
-      console.log("Number is not 2");
-      // Perform the desired action for numbers other than 2
+
+  useEffect(()=>{
+    const interval = setInterval(()=>{
+      setSeconds((prevSeconds)=>{
+        if (prevSeconds === 0) {
+          clearInterval(interval);
+          return prevSeconds;
+      }
+      return prevSeconds - 1;
+    } );
+    }, 1000);
+
+    return ()=>{
+      clearInterval(interval);
     }
-    
-    console.log(currentPath)
+  }, []);
    
-  return (
-    <div>
-      helloooooooooooo
+  return(
+    <div className={styles.candidate_message_container}>
+      <div className={styles.bg}>
+      <div className={styles.contain}>
+        <CandidateShowMessage header={heading} description={description} />
+        <div className={`${seconds !== 0 ? styles.timer : ""}`}>{seconds !== 0 ? seconds: ""}</div>
+        
+        <p className={styles.candidate_agreement}>{uploadMessage}</p>
+        
+       
+        
+      </div>
+      </div>
+      
     </div>
-  )
+  );
 }
 
 export default EndInterview
