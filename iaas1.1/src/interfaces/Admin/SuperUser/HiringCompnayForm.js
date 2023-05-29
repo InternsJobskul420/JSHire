792f3f85fa37238e073eb604170361882cb04ecc
nishiteshSomething <nishiteshpadhi@gmail.com>
2023-05-29 10:51:15 +0530

    new changes

--- iaas1.1/src/interfaces/Admin/SuperUser/HiringCompnayForm.js
+++ iaas1.1/src/interfaces/Admin/SuperUser/HiringCompnayForm.js
@@ -6,12 +6,18 @@ export const HiringCompnayForm = () => {
   return (
     <>
     <NavigationBar/>
-    <div className={styles.formContainer}>
-      <h1 className={styles.formHeading}>Create An Account</h1>
+    {/* <div className={styles.formContainer}>
+      <h1 className={styles.formHead
+        ing}>Create An Account</h1>
+      <div className={styles.containerhcform}>
       <form className={styles.form}>
         <div className={styles.formGroup}>
           <label htmlFor="companyName">Company Name</label>
           <input type="text" id="companyName" className={styles.formInput} />
+        </div>
+        <div className={styles.formGroup}>
+          <label htmlFor="companyName">Company Description</label>
+          <input type="text" id="companyDescription" className={styles.formInput} />
         </div>
         <div className={styles.formGroup}>
           <label htmlFor="email">Email</label>
@@ -31,7 +37,49 @@ export const HiringCompnayForm = () => {
         </div>
         <button type="submit" className={styles.formButton}>Add Company</button>
       </form>
-    </div>
+      </div>
+    </div> */}
+          <div className={styles.container}>
+        <form className={styles.form}>
+          <div className={styles.column}>
+          <div className={styles.formGroup}>
+          <label htmlFor="companyName">Company Name</label>
+          <input type="text" id="companyName" required className={styles.formInput} />
+        </div>
+            <div className={styles.formGroup}>
+              <label htmlFor="company-description">Company Description </label>
+              <textarea
+                id="company-description"
+                name="company-description"
+                required
+                minLength="10"
+              ></textarea>
+            </div>
+          </div>
+          <div className={styles.column}>
+          <div className={styles.formGroup}>
+        <label htmlFor="phoneNumber">Phone Number</label>
+        <input
+          type="number"
+          id="quantity"
+          name="quantity"
+        className={styles.formInput}/>
+        </div>
+        <div className={styles.formGroup}>
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" required className={styles.formInput} />
+        </div>
+        <div className={styles.formGroup}>
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" required className={styles.formInput} />
+        </div>
+            <div className={styles.formGroup}>
+            <button type="submit" className={styles.formButton}>Add Company</button>
+            </div>
+          </div>
+        </form>
+      </div>
+
     </>
   )
 }
