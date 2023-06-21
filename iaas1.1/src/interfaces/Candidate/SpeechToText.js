eb2952bdd07a4a543e102130a3c59fed9ae51f4f
Ishita <ishitamun2000@gmail.com>
2023-06-21 10:44:45 +0530

    corrected

--- iaas1.1/src/interfaces/Candidate/SpeechToText.js
+++ iaas1.1/src/interfaces/Candidate/SpeechToText.js
@@ -1,1 +1,30 @@
+import React from 'react'
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+import { createSpeechlySpeechRecognition } from '@speechly/speech-recognition-polyfill';
 
+
+
+
+const SpeechToText = () => {
+
+    const { transcript, browserSupportsSpeechRecognition } = useSpeechRecognition();
+    const startListening = ()=> SpeechRecognition.startListening({ continuous: true, language: "en-IN" });
+    const stopListening =()=> SpeechRecognition.stopListening();
+
+    if (!browserSupportsSpeechRecognition) {
+        return null
+      }
+
+
+  return (
+    <>
+    <div>SpeechToText</div>
+    <button onClick={startListening}> start</button>
+    <button onClick={stopListening}>stop</button>
+    <div>hello{transcript}</div>
+    </>
+    
+  )
+}
+
+export default SpeechToText
